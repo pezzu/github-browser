409d760832f1f7d2320dff54bc60baa05dbe5644
pezzu <7243796+pezzu@users.noreply.github.com>
2020-10-06 15:45:24 -0400

    Styling for first page added

--- src/Home.js
+++ src/Home.js
@@ -15,18 +15,25 @@ const Home = (props) => {
     }
 
     return (
-        <div>
-            <h3>Enter user id</h3>
-            <input id="input-field"
-                type = "text"
-                value = {user}
-                onChange = {onChangeUser}
-            />
-            <button id="search-button"
-                onClick = {onSearch}
-            >
-                Search
-            </button>
+        <div className = "flex justify-center items-center h-screen px-8 py-8">
+            <div className = "px-4">
+                <input id = "input-field"
+                    className = "shadow appearance-none border rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" 
+                    placeholder = "Enter github user ID"                        
+                    type = "text"
+                    value = {user}
+                    onChange = {onChangeUser}
+                />
+            </div>
+            <div className = "px-4">
+                <button id = "search-button"
+                    className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                    type = "button"
+                    onClick = {onSearch}
+                >
+                    Search
+                </button>
+            </div>
         </div>
     )
 }
